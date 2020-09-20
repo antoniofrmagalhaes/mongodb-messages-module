96b12492198a93134aa34dbcbe662d8cd4ba7033
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-20 19:58:42 -0400

    feat: dependencies container setup

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './core/app';
 import appConfig from './core/config';
 
 /** Dependencies injection container */
-// import './container';
+import './container';
 
 app.listen(appConfig.port, () =>
   console.log('Api running on port %s.', appConfig.port),
