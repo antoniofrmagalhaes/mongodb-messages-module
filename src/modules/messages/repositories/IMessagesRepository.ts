e3cff0be84ce773291f5e3750c8c2409abdd922e
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-20 20:13:13 -0400

    feat: find messages method created

--- src/modules/messages/repositories/IMessagesRepository.ts
+++ src/modules/messages/repositories/IMessagesRepository.ts
@@ -1,6 +1,11 @@
 import ICreateMessageDTO from '../DTO/ICreateMessageDTO';
 import { IMessageDocument } from '../infra/mongoose/entities/schemas/Message';
+import {
+  IQuery,
+  IResponse,
+} from '../infra/mongoose/repositories/MessagesRepository';
 
 export default interface IMessagesRepository {
+  find(query: IQuery): Promise<IResponse>;
   create(notificationData: ICreateMessageDTO): Promise<IMessageDocument>;
 }
