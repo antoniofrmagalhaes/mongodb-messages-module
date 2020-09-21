516e756a430d319de5c41eacd1ac1ea1ec5c0fb1
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-20 20:02:11 -0400

    feat: messages routes setup

--- src/core/routes/index.ts
+++ src/core/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import messagesRouter from '../../modules/messages/infra/http/routes/messages.routes';
+
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ status: 'ok' });
 });
+
+routes.use('/messages', messagesRouter);
 
 export default routes;
