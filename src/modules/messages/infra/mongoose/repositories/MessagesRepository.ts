54aa4e735321c36371083d870148baa7d87f49c8
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-20 20:14:49 -0400

    feat: public find method created at repository class

--- src/modules/messages/infra/mongoose/repositories/MessagesRepository.ts
+++ src/modules/messages/infra/mongoose/repositories/MessagesRepository.ts
@@ -1,7 +1,49 @@
 import Message, { IMessageDocument } from '../entities/schemas/Message';
 import ICreateMessageDTO from '../../../DTO/ICreateMessageDTO';
 
+export interface IQuery {
+  page?: string;
+  unread?: string;
+}
+
+export interface IResponse {
+  total: number;
+  unread: number;
+  result: IMessageDocument[];
+}
+
 class MessagesRepository {
+  public async find(query: IQuery): Promise<IResponse> {
+    const { page, unread } = query;
+
+    const totalRecords = await Message.estimatedDocumentCount();
+    const totalUnread = await Message.countDocuments({ read: false });
+
+    if (unread) {
+      const unreadOnly = await Message.find()
+        .where('read')
+        .equals(false)
+        .limit(10)
+        .skip((Number(page) - 1) * 10)
+        .sort('-createdAt');
+      return {
+        total: totalRecords,
+        unread: totalUnread,
+        result: unreadOnly,
+      };
+    }
+
+    const messages = await Message.find()
+      .limit(10)
+      .skip((Number(page) - 1) * 10)
+      .sort('-createdAt');
+    return {
+      total: totalRecords,
+      unread: totalUnread,
+      result: messages,
+    };
+  }
+
   public async create({
     from,
     title,
